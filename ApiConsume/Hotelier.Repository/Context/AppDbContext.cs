bed964a01d1eaa4f9ca876ec26de96fc218cae9b
Orçun Aşık <asikorcun@gmail.com>
2023-10-26 01:29:22 +0300

    AppDbContext structure was prepared for Db connection.

---
+++ ApiConsume/Hotelier.Repository/Context/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Hotelier.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Hotelier.Repository.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<HotelService> HotelServices { get; set; }
+    public DbSet<Staff> Staffs { get; set; }
+    public DbSet<Testimonial> Testimonials { get; set; }
+    public DbSet<EmailSubscribe> EmailSubscribes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
