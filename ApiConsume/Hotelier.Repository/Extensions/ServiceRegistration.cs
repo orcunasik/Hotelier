045e98c9bb544e3534b58a4f8b4048db36b50982
Orçun Aşık <asikorcun@gmail.com>
2023-10-29 23:54:26 +0300

    Added service registration class to report repositories to IoC.

---
+++ ApiConsume/Hotelier.Repository/Extensions/ServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using Hotelier.Core.Repositories.Abstracts;
+using Hotelier.Repository.Context;
+using Hotelier.Repository.Repositories.Concretes.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hotelier.Repository.Extensions;
+
+public static class ServiceRegistration
+{
+    public static void LoadMyRepositories(this IServiceCollection services)
+    {
+        services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.ConnectionString));
+
+        services.AddScoped<IStaffRepository, EFStaffRepository>();
+        services.AddScoped<IRoomRepository, EFRoomRepository>();
+        services.AddScoped<IEmailSubscribeRepository, EFEmailSubscribeRepository>();
+        services.AddScoped<IHotelServiceRepository, EFHotelServiceRepository>();
+        services.AddScoped<ITestimonialRepository, EFTestimonialRepository>();
+
+    }
+}
