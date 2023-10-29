926b3c6524f56c354a9959a421be7e781d15efb2
Orçun Aşık <asikorcun@gmail.com>
2023-10-29 23:58:15 +0300

    Added service registration class to report services to IoC.

---
+++ ApiConsume/Hotelier.Service/Extensions/ServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Hotelier.Core.Services.Abstracts;
+using Hotelier.Service.Managers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hotelier.Service.Extensions;
+
+public static class ServiceRegistration
+{
+    public static void LoadMyServices(this IServiceCollection services)
+    {
+        services.AddScoped<IStaffService, StaffManager>();
+        services.AddScoped<IRoomService, RoomManager>();
+        services.AddScoped<IEmailSubscribeService, EmailSubscribeManager>();
+        services.AddScoped<IHotelServiceService, HotelServiceManager>();
+        services.AddScoped<ITestimonialService, TestimonialManager>();
+    }
+}
