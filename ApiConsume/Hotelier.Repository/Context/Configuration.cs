5292332f6ea4c2a8f3d13d4d936270f3a8365f2e
Orçun Aşık <asikorcun@gmail.com>
2023-10-29 23:49:11 +0300

    DesignTime configuration has been added to avoid errors in migration process

---
+++ ApiConsume/Hotelier.Repository/Context/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Hotelier.Repository.Context;
+
+static class Configuration
+{
+    public static string ConnectionString
+    {
+        get
+        {
+            ConfigurationManager configurationManager = new();
+            configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../Hotelier.WebApi/"));
+            configurationManager.AddJsonFile("appsettings.json");
+            return configurationManager.GetConnectionString("SqlConnection");
+        }
+            
+    }
+}
---
+++ ApiConsume/Hotelier.Repository/Context/DesignTimeDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Hotelier.Repository.Context;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        DbContextOptionsBuilder optionsBuilder = new();
+        optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+        return new(optionsBuilder.Options);
+    }
+}
